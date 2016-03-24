d93133e955cc5b29b63b2d22376402d11f771369
Zeng Luyang <zengluyang@gmail.com>
2016-03-24 21:32:11 +0800

    ADD: Polygon

--- ParsePoint/src/com/ifc/jyg/CoordinateOfPoint.java
+++ ParsePoint/src/com/ifc/jyg/CoordinateOfPoint.java
@@ -11,7 +11,14 @@ public class CoordinateOfPoint implements Comparable<Object>{
 	public CoordinateOfPoint() {
 		
 	}
-	
+
+	public CoordinateOfPoint(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+
+	}
+
 	public void setX(double x) {
 		this.x = x;
 	} 
