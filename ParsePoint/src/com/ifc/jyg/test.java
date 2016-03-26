73aa5e8771fe4702e95f09da48a15ce8d3e0f12f
jiangyanggen <jiangyanggen@163.com>
2016-03-26 19:16:21 +0800

    modify ParseObjFile

--- ParsePoint/src/com/ifc/jyg/test.java
+++ ParsePoint/src/com/ifc/jyg/test.java
@@ -4,17 +4,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 
 public class test {
  
 	public static void main(String[] args) throws IOException {
-<<<<<<< HEAD
 		ParseObjFile parseObjFile = new ParseObjFile("E:\\IFC\\IFCFile\\YD_S_B04_1F.obj"); 
-=======
-		ParseObjFile parseObjFile = new ParseObjFile("E:\\IFC\\IFCFile\\whole.obj");
-		Map<Double, ArrayList<Triangle>> samevalueMap = new HashMap<>();
->>>>>>> d23e2b7d80ad72b569c0626c5f684000ee6e488a
 		IntersectRectangle ir = new IntersectRectangle();  
 		ArrayList<Cuboid> listCuboids = parseObjFile.getCuboid(); 
 		System.out.println("listCuboids: " + listCuboids.size());
@@ -49,25 +45,7 @@ public class test {
 
 		}
 
-		Map<Map<Rectangle, Rectangle>, String> testMap = ir.getIntersectMap();
-<<<<<<< HEAD
 
-		System.out.println("testMap.size() : " + testMap.size());
-		int cnt = 0;
-		for (Map<Rectangle, Rectangle> rectMap : testMap.keySet()) {
-			for (Rectangle rectangle : rectMap.keySet()) {
-				if(rectMap.get(rectangle)!=null) {
-					cnt++;
-					Rectangle a = rectangle;
-					Rectangle b = rectMap.get(rectangle);
-//					System.out.print(a);
-//					System.out.print(b);
-//
-//					Polygon polygon = new Polygon(a,b);
-//					System.out.println(polygon.getEdgeList());
-//					System.out.println(polygon.getPointList());
-//					System.out.println("###############");
-=======
 		ArrayList<ArrayList<TreeSet<Rectangle>>> intersectResult = ir.getPartitionResult();
 		//System.out.println("intersectResult "+intersectResult);
 		System.out.println("intersectResult.size() : " + intersectResult.size());
@@ -79,7 +57,7 @@ public class test {
 				System.out.println(String.format("title('%s %f');\n",Rectangle.directionString[recSet.first().getDirection()],recSet.first().getIntersectvalue()));
 				for(Rectangle r:recSet) {
 					System.out.println(r.toMatlab2d());
->>>>>>> d23e2b7d80ad72b569c0626c5f684000ee6e488a
+
 				}
 				System.out.println("%###############\n");
 			}
