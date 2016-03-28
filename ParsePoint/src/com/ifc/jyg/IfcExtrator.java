f43ab7f2426f2b19355a9df4e9bc759c1726eed5
Zeng Luyang <zengluyang@gmail.com>
2016-03-29 00:18:53 +0800

    GUI

---
+++ ParsePoint/src/com/ifc/jyg/IfcExtrator.java
@@ -0,0 +1,249 @@
+package com.ifc.jyg;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Created by ZLY on 2016/3/28.
+ */
+public class IfcExtrator {
+    private String finalMatlabFile;
+    private String finalResultFile;
+    private String ifcConvertExeName;
+    private String inputIfcFileName;
+    private ArrayList<Rectangle> recRlt = new ArrayList<Rectangle>();
+    private ArrayList<Polygon> polyRlt = new ArrayList<Polygon>();
+    private PrintWriter outMatlab;
+    private PrintWriter outFinalResult;
+    public String successMessage;
+
+    public String getIfcConvertExeName() {
+        return ifcConvertExeName;
+    }
+
+    public String getInputIfcFileName() {
+        return inputIfcFileName;
+    }
+
+    public ArrayList<Rectangle> getRecRlt() {
+        return recRlt;
+    }
+
+    public ArrayList<Polygon> getPolyRlt() {
+        return polyRlt;
+    }
+
+    public void setInputIfcFileName(String inputIfcFileName) {
+        this.inputIfcFileName = inputIfcFileName;
+    }
+
+    public void setIfcConvertExeName(String ifcConvertExeName) {
+        this.ifcConvertExeName = ifcConvertExeName;
+    }
+
+    private ParseObjFile generateObjFileAndGetPaser(String inputIfcFileName, int type) {
+        String ifcType = Cuboid.typeIfcString[type];
+        File inputIfcFile = new File(inputIfcFileName);
+        String outputObjFileName = inputIfcFile.getName() + "_" + ifcType + ".obj";
+        File outputObjFile = new File(outputObjFileName);
+        if (outputObjFile.lastModified() < inputIfcFile.lastModified()) {
+            System.out.println("Objfile " + outputObjFileName + " out-of-date, converting using " + ifcConvertExeName);
+            StringBuilder ifcConverteExePathSb = new StringBuilder();
+
+            ifcConverteExePathSb
+                    .append("cmd /c ").append(ifcConvertExeName).append(" ")
+                    .append(inputIfcFileName).append(" ")
+                    .append(outputObjFileName).append(" ")
+                    .append("--use-world-coords ")
+                    .append("--weld-vertices ")
+                    .append("--include --entities ").append(ifcType);
+
+            try {
+                String line;
+                Process p = Runtime.getRuntime().exec(ifcConverteExePathSb.toString());
+                StreamGobbler errorGobbler = new StreamGobbler(p.getErrorStream(), "ERROR");
+
+                // any output?
+                StreamGobbler outputGobbler = new StreamGobbler(p.getInputStream(), "OUTPUT");
+                errorGobbler.start();
+                int exitVal = p.waitFor();
+                p.waitFor();
+            } catch (Exception err) {
+                err.printStackTrace();
+            }
+        } else {
+            System.out.println("Objfile " + inputIfcFileName + " up-to-date.");
+        }
+        return new ParseObjFile(outputObjFileName, type);
+
+    }
+
+    public IfcExtrator() throws IOException {
+        this.ifcConvertExeName = ".\\IfcConvert.exe";
+        this.inputIfcFileName = ".\\YD_S_B04_1F.ifc";
+        this.finalResultFile = inputIfcFileName+"_final_result.txt";
+        this.finalMatlabFile = inputIfcFileName+"_final_Matlab_file.m";
+        BufferedWriter brMatlab = new BufferedWriter(new FileWriter(finalMatlabFile));
+        outMatlab = new PrintWriter (brMatlab);
+        BufferedWriter brFinalResult = new BufferedWriter(new FileWriter(finalResultFile));
+        outFinalResult = new PrintWriter (brFinalResult);
+    }
+
+    public void extract() throws IOException {
+
+        ParseObjFile parseBeamObjFile = this.generateObjFileAndGetPaser(inputIfcFileName,Cuboid.BEAM);
+        ParseObjFile parseColumnObjFile = this.generateObjFileAndGetPaser(inputIfcFileName,Cuboid.COLUMN);
+        ParseObjFile parseSlabObjFile = this.generateObjFileAndGetPaser(inputIfcFileName,Cuboid.POLYSLAB);
+
+        IntersectRectangle ir = new IntersectRectangle();
+        ArrayList<Cuboid> listCuboidsBeam = parseBeamObjFile.getCuboid();
+        ArrayList<Cuboid> listCuboidsColumn = parseColumnObjFile.getCuboid();
+        ArrayList<Cuboid> listCuboidsSlab = parseSlabObjFile.getCuboid();
+        ArrayList<Polyhedron> listPolyhedron = parseSlabObjFile.getSlabPolys();
+        ArrayList<Cuboid> listCuboids= new ArrayList<Cuboid>();
+        listCuboids.addAll(listCuboidsBeam);
+        listCuboids.addAll(listCuboidsColumn);
+        listCuboids.addAll(listCuboidsSlab);
+
+        for (int j = 0; j < listCuboids.size(); j++) {
+            Cuboid cuboid = listCuboids.get(j);
+            Set<Rectangle> neededRecs = cuboid.getNeededRectangels();
+            if(neededRecs!=null) {
+                for(Rectangle r : neededRecs) {
+                    ir.addRectangleTogether(r);
+                }
+            }
+            //}
+        }
+
+        ArrayList<Polyhedron> ps = new ArrayList<Polyhedron>();
+        ps.addAll(listPolyhedron);
+        int slabNeededRecCnt = 0;
+        int i=0;
+        for (Polyhedron p:ps) {
+            i++;
+
+            polyRlt.add(p.getDownPolygon());
+            outMatlab.println(String.format("figure(%d);\n",i));
+            outMatlab.println(String.format("title('%s %f %s');\n",Rectangle.directionString[p.getDownPolygon().getDirection()],p.height,p.Id));
+            outMatlab.println(p.getDownPolygon().toMatlab2D());
+            outMatlab.println("%###############\n");
+            ArrayList<Rectangle> recs = p.getNeededRectangles();
+
+            for(Rectangle r:recs) {
+
+                ir.addRectangleTogether(r);
+                slabNeededRecCnt++;
+            }
+
+        }
+
+
+
+        ArrayList<ArrayList<TreeSet<Rectangle>>> intersectResult = ir.getPartitionResult();
+        int total_cnt=0;
+        for(ArrayList<TreeSet<Rectangle>> recSetList:intersectResult) {
+            for(TreeSet<Rectangle> recSet:recSetList) {
+                if(recSet.size()==1) {
+                    i++;
+                    recSet.size();
+                    Rectangle rec = recSet.first();
+                    recRlt.add(rec);
+                    if(rec.Id!=null && rec.Id.equals("BEA_____:200_x350mm:736737")) {
+                        rec.toMatlab2d();
+                    }
+                    outMatlab.println(String.format("figure(%d);\n",i));
+                    outMatlab.println(String.format("title('%s %f %s');\n",Rectangle.directionString[rec.getDirection()],rec.getIntersectvalue(),rec.Id));
+                    outMatlab.println(rec.toMatlab2d());
+                    outMatlab.println("%###############\n");
+                } else {
+                    Rectangle rec = recSet.first();
+                    if(rec.Id!=null && rec.Id.equals("COL_____:600_x_600mm:566092")) {
+                        rec.toMatlab2d();
+                    }
+                    Rectangle big = recSet.pollFirst();
+                    if(big!=null&&recSet.size()!=0) {
+                        ArrayList<Polygon> polygons = Rectangle.contrunctPolygonsUsingBigRectangleAndSmallRectangles(big, new ArrayList<Rectangle>(recSet));
+                        for(Polygon p: polygons) {
+                            i++;
+                            polyRlt.add(p);
+                            outMatlab.println(String.format("figure(%d);\n",i));
+                            outMatlab.println(String.format("title('%s %f %s');\n",Rectangle.directionString[big.getDirection()],big.getIntersectvalue(),p.Id));
+                            outMatlab.println(p.toMatlab2D());
+                        }
+                        outMatlab.println("%###############\n");
+
+                    }
+
+                }
+            }
+            outMatlab.println("%!!!!!!!!!!!!!!!!!\n");
+        }
+        outMatlab.println("%i "+i);
+
+        outFinalResult.println("############ Generated by IfcSCBModler at "+new Date().toString()+"############");
+        outFinalResult.println("############ "+recRlt.size()+" Rectangles");
+        outFinalResult.println("############ "+polyRlt.size()+" Polygons");
+    }
+
+    public void printResultToFile() {
+
+        for(Rectangle r:recRlt) {
+            outFinalResult.println(String.format("REC %s",r.Id));
+            outFinalResult.println(String.format("V %f %f %f",r.topLeft.getX(),r.topLeft.getX(),r.topLeft.getZ()));
+            outFinalResult.println(String.format("V %f %f %f",r.topRight.getX(),r.topRight.getX(),r.topRight.getZ()));
+            outFinalResult.println(String.format("V %f %f %f",r.downRight.getX(),r.downRight.getX(),r.downRight.getZ()));
+            outFinalResult.println(String.format("V %f %f %f",r.downLeft.getX(),r.downLeft.getX(),r.downLeft.getZ()));
+        }
+        for(Polygon p:polyRlt) {
+            outFinalResult.println(String.format("POL %s",p.Id));
+            for(CoordinateOfPoint cop:p.getPointList()) {
+                if(cop!=null)
+                    outFinalResult.println(String.format("V %f %f %f",cop.getX(),cop.getX(),cop.getZ()));
+            }
+            for(Edge e:p.getEdgeList()) {
+                outFinalResult.println(String.format("E %f %f %f %f %f %f",
+                        e.getFirst().getX(),e.getFirst().getX(),e.getFirst().getZ(),
+                        e.getSecond().getX(),e.getSecond().getX(),e.getSecond().getZ()
+                ));
+            }
+        }
+        successMessage = "Created results at: "+finalMatlabFile+" and "+finalResultFile;
+        System.out.println(successMessage);
+
+    }
+
+
+}
+
+class StreamGobbler extends Thread
+{
+    InputStream is;
+    String type;
+
+    StreamGobbler(InputStream is, String type)
+    {
+        this.is = is;
+        this.type = type;
+    }
+
+    public void run()
+    {
+        try
+        {
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line=null;
+            while ( (line = br.readLine()) != null)
+                System.out.println("%"+type + ">" + line);
+        } catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+    }
+}
+
+
---
+++ ParsePoint/src/com/ifc/jyg/NewFrame.java
@@ -0,0 +1,179 @@
+package com.ifc.jyg;
+
+/**
+ * Created by ZLY on 2016/3/28.
+ */
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.io.File;
+
+/**
+ * swing基础实例
+ * @author HZ20232
+ *
+ */
+class NewFrame extends JFrame{
+    private JLabel label1;
+    private JButton button1;
+    private JTextField text1;
+    private JComboBox box;
+    private JMenuBar menuBar;
+    private JSlider slider;
+    private JSpinner spinner;
+    private JToolBar toolBar;
+    public JFileChooser exeFileChooser;
+    public JFileChooser inputFileChooser;
+    public NewFrame(){
+        super();
+
+
+        //this.getContentPane().setLayout(new FlowLayout());//设置布局控制器
+
+//        this.getContentPane().setLayout(new GridLayout(1,2));//设置布局控制器,需要给出设定的行列数目
+
+//        this.getContentPane().setLayout(new BorderLayout());//设置布局控制器，以North,South,West,East，来控制控件布局
+
+        this.getContentPane().setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS));//设置布局控制器
+
+
+//        this.add(this.getBox(),null);//添加下拉列表框
+        this.setTitle("Hello World!");//设置窗口标题exeFileChooser = new JFileChooser();
+        JButton button = new JButton("选择EXE");
+        button.setBounds(103,110,71,27);
+        button.setText("选择EXE");
+        button.setToolTipText("选择EXE");
+        exeFileChooser = new JFileChooser(".\\");
+        inputFileChooser = new JFileChooser(".\\");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                exeFileChooser.showOpenDialog(null);
+            }
+        });
+        JButton button2 = new JButton("选择IFC");
+        button2.setBounds(103,110,71,27);
+        button2.setText("选择IFC");
+        button2.setToolTipText("选择IFC");
+        this.add(button2);
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inputFileChooser.showOpenDialog(null);
+            }
+        });
+        this.add(button);        this.add(this.getButton());//添加按钮
+        this.add(this.getLabel());//添加标签
+
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = 500;
+        int height = 500;
+        this.setBounds((d.width - width) / 2, (d.height - height) / 2, width, height);
+        this.setSize(1000,200);
+
+    }
+
+    public JFileChooser getExeFileChooser() {
+        return exeFileChooser;
+    }
+
+    public JToolBar getToolBar(){
+        if(toolBar==null){
+            toolBar = new JToolBar();
+            toolBar.setBounds(103,260,71,20);
+            toolBar.setFloatable(true);
+        }
+        return toolBar;
+    }
+    public JSpinner getSpinner(){
+        if(spinner==null){
+            spinner = new JSpinner();
+            spinner.setBounds(103,220, 80,20);
+            spinner.setValue(100);
+        }
+        return spinner;
+    }
+    public JSlider getSlider(){
+        if(slider==null){
+            slider = new JSlider();
+            slider.setBounds(103,200,100, 20);
+            slider.setMaximum(100);
+            slider.setMinimum(0);
+            slider.setOrientation(0);
+            slider.setValue(0);
+        }
+        return slider;
+    }
+
+    /**
+     * 设置下拉列表框
+     * @return
+     */
+    public JComboBox getBox(){
+        if(box==null){
+            box = new JComboBox();
+            box.setBounds(103,140,71,27);
+            box.addItem("1");
+            box.addItem("2");
+            box.addActionListener(new comboxListener());//为下拉列表框添加监听器类
+
+        }
+        return box;
+    }
+    private class comboxListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            Object o = e.getSource();
+            System.out.println(o.toString());
+        }
+    }
+    /**
+     * 设置标签
+     * @return 设置好的标签
+     */
+    public JLabel getLabel(){
+        if(label1==null){
+            label1 = new JLabel();
+            label1.setBounds(34,49,53,18);
+            label1.setText("Name");
+            label1.setToolTipText("JLabel");
+        }
+        return label1;
+    }
+    /**
+     * 设置按钮
+     * @return 设置好的按钮
+     */
+    public JButton getButton(){
+        if(button1==null){
+            button1 = new JButton();
+            button1.setBounds(103,110,71,27);
+            button1.setText("提取");
+            button1.setToolTipText("提取");
+            //button1.addActionListener(new HelloButton());//添加监听器类，其主要的响应都由监听器类的方法实现
+
+        }
+        return button1;
+    }
+    /**
+     * 监听器类实现ActionListener接口，主要实现actionPerformed方法
+     * @author HZ20232
+     *
+     */
+    private class HelloButton implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            System.out.println("Hello world!");
+        }
+    }
+    /**
+     * 设定文本域
+     * @return
+     */
+    public JTextField getTextField(){
+        if(text1==null){
+            text1 = new JTextField();
+            text1.setBounds(96,49,160,20);
+        }
+        return text1;
+    }
+}
