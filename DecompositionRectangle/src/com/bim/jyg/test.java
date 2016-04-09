ac6b533ccb5834eff4ec26d160ac49b23394d6d5
jiangyanggen <jiangyanggen@163.com>
2016-04-09 22:22:08 +0800

    add indicate input standard length

--- DecompositionRectangle/src/com/bim/jyg/test.java
+++ DecompositionRectangle/src/com/bim/jyg/test.java
@@ -2,33 +2,40 @@ package com.bim.jyg;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class test {
 
+	private static Scanner cin;
+
 	public static void main(String[] args) {
-		CoordinateOfPoint topLeft = new CoordinateOfPoint(1, 0, 0);
+		CoordinateOfPoint topLeft = new CoordinateOfPoint(0, 0, 0);
 		CoordinateOfPoint downRight = new CoordinateOfPoint(956, 1059, 0);
-		Rectangle rectangle = new Rectangle(topLeft, downRight, "123456789");
-		DecompositionRectangle decompositionRectangle = new DecompositionRectangle(rectangle);
-		ArrayList<Edge> newPolygonEdges = new ArrayList<Edge>();
-		//100 indiacate standard length width is 50(default), 40 is standard number ,not effect temporary 
-		ArrayList<CoordinateOfPoint> newPolygonPoints = decompositionRectangle.getNewPolygonPoints(100, 40);
-		if (newPolygonPoints != null) {
-//			System.out.println("newPolygonPoints.size()" + newPolygonPoints.size());
-			for (CoordinateOfPoint coordinateOfPoint : newPolygonPoints) {
-				System.out.println(coordinateOfPoint.toString());
+		Rectangle rectangle = new Rectangle(topLeft, downRight, "123456789"); 
+		cin = new Scanner(System.in);
+		while (cin.hasNext()) {
+			double length = cin.nextDouble();
+			DecompositionRectangle decompositionRectangle = new DecompositionRectangle(rectangle);
+			ArrayList<Edge> newPolygonEdges = new ArrayList<Edge>();
+			ArrayList<CoordinateOfPoint> newPolygonPoints = decompositionRectangle.getNewPolygonPoints(length, 40);
+			if (newPolygonPoints != null) {
+//				System.out.println("newPolygonPoints.size()" + newPolygonPoints.size());
+				for (CoordinateOfPoint coordinateOfPoint : newPolygonPoints) {
+					System.out.println(coordinateOfPoint.toString());
+				}
+			}
+			
+			 
+			for (int i = 0; i < (newPolygonPoints.size() / 2); i++) {
+				Edge edge = new Edge(newPolygonPoints.get(i), newPolygonPoints.get(i+1));
+				newPolygonEdges.add(edge);
+			}
+			
+			for (Edge edge : newPolygonEdges) {
+				System.out.print(edge.toString());
 			}
 		}
 		
-		 
-		for (int i = 0; i < (newPolygonPoints.size() / 2); i++) {
-			Edge edge = new Edge(newPolygonPoints.get(i), newPolygonPoints.get(i+1));
-			newPolygonEdges.add(edge);
-		}
-		
-		for (Edge edge : newPolygonEdges) {
-			System.out.print(edge.toString());
-		}
 		 
 	}
 }
